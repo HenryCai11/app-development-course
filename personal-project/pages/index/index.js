381668d3f7f4476aa090f9f743e1dafa634301b0
Henry <henry_f_miller@163.com>
2020-04-26 13:20:42 +0800

    initialization

---
+++ personal-project/pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
+Page({
+  data: {
+    years: years,
+    months: months,
+    days: days
+  }
+})
